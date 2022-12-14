dcac7dff35e73f73ec702eba94dfcf68c97bc016
Naved Ansari <naved001@gmail.com>
2022-12-14 13:04:53 -0500

    Format the file using Black

--- src/coldfront_plugin_cloud/management/commands/validate_allocations.py
+++ src/coldfront_plugin_cloud/management/commands/validate_allocations.py
@@ -5,10 +5,8 @@ from coldfront_plugin_cloud import openstack
 from coldfront_plugin_cloud import utils
 
 from django.core.management.base import BaseCommand
-from coldfront.core.resource.models import (Resource,
-                                            ResourceType)
-from coldfront.core.allocation.models import (Allocation,
-                                              AllocationStatusChoice)
+from coldfront.core.resource.models import Resource, ResourceType
+from coldfront.core.allocation.models import Allocation, AllocationStatusChoice
 from keystoneauth1.exceptions import http
 
 
@@ -16,49 +14,51 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = 'Validates quotas and users in resource allocations.'
+    help = "Validates quotas and users in resource allocations."
 
     def add_arguments(self, parser):
-        parser.add_argument('--apply', action='store_true',
-                            help='Apply expected state if validation fails.')
+        parser.add_argument(
+            "--apply",
+            action="store_true",
+            help="Apply expected state if validation fails.",
+        )
 
     def handle(self, *args, **options):
         supported_resources = Resource.objects.filter(
-            resource_type=ResourceType.objects.get(
-                name='OpenStack'
-            )
+            resource_type=ResourceType.objects.get(name="OpenStack")
         )
         allocations = Allocation.objects.filter(
             resources__in=supported_resources,
-            status=AllocationStatusChoice.objects.get(name='Active')
+            status=AllocationStatusChoice.objects.get(name="Active"),
         )
         for allocation in allocations:
             allocation_str = f'{allocation.pk} of project "{allocation.project.title}"'
-            msg = f'Starting resource validation for allocation {allocation_str}.'
+            msg = f"Starting resource validation for allocation {allocation_str}."
             logger.debug(msg)
 
             failed_validation = False
 
             allocator = openstack.OpenStackResourceAllocator(
-                allocation.resources.first(),
-                allocation
+                allocation.resources.first(), allocation
             )
 
             project_id = allocation.get_attribute(attributes.ALLOCATION_PROJECT_ID)
             if not project_id:
-                logger.error(f'{allocation_str} is active but has no Project ID set.')
+                logger.error(f"{allocation_str} is active but has no Project ID set.")
                 continue
 
             try:
                 allocator.identity.projects.get(project_id)
             except http.NotFound:
-                logger.error(f'{allocation_str} has Project ID {project_id}. But'
-                             f' no project found in OpenStack.')
+                logger.error(
+                    f"{allocation_str} has Project ID {project_id}. But"
+                    f" no project found in OpenStack."
+                )
                 continue
 
             quota = allocator.get_quota(project_id)
             for attr in attributes.ALLOCATION_QUOTA_ATTRIBUTES:
-                if 'OpenStack' in attr:
+                if "OpenStack" in attr:
                     key = openstack.QUOTA_KEY_MAPPING_ALL_KEYS.get(attr, None)
                     if not key:
                         # Note(knikolla): Some attributes are only maintained
@@ -69,22 +69,28 @@ class Command(BaseCommand):
                     expected_value = allocation.get_attribute(attr)
                     current_value = quota.get(key, None)
                     if expected_value is None and current_value:
-                        msg = (f'Attribute "{attr}" expected on allocation {allocation_str} but not set.'
-                               f' Current quota is {current_value}.')
-                        if options['apply']:
+                        msg = (
+                            f'Attribute "{attr}" expected on allocation {allocation_str} but not set.'
+                            f" Current quota is {current_value}."
+                        )
+                        if options["apply"]:
                             utils.set_attribute_on_allocation(
                                 allocation, attr, current_value
                             )
-                            msg = f'{msg} Attribute set to match current quota.'
+                            msg = f"{msg} Attribute set to match current quota."
                         logger.warning(msg)
                     elif not current_value == expected_value:
                         failed_validation = True
-                        msg = (f'Value for quota for {attr} = {current_value} does not match expected'
-                               f' value of {expected_value} on allocation {allocation_str}')
+                        msg = (
+                            f"Value for quota for {attr} = {current_value} does not match expected"
+                            f" value of {expected_value} on allocation {allocation_str}"
+                        )
                         logger.warning(msg)
 
-            if failed_validation and options['apply']:
+            if failed_validation and options["apply"]:
                 allocator.set_quota(
                     allocation.get_attribute(attributes.ALLOCATION_PROJECT_ID)
                 )
-                logger.warning(f'Quota for allocation {allocation_str} was out of date. Reapplied!')
+                logger.warning(
+                    f"Quota for allocation {allocation_str} was out of date. Reapplied!"
+                )
